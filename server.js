d620311c591895de1885147ee000a1ff1409e03d
Mike Evlantev <mikeev21@gmail.com>
2019-11-05 22:54:25 -0800

    express server set up

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const users = require("./userRepo");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/users", users.getUsers);
+app.get("/users/:id", users.getUserById);
+app.post("/users", users.createUser);
+app.put("/users/:id", users.updateUser);
+app.delete("/users/:id", users.deleteUser);
+
+app.get("/", (req, res) => {
+  res.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
