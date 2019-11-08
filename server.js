4ed137482fdf35b61d352001d08540bcce27c04a
Mike Evlantev <mikeev21@gmail.com>
2019-11-07 20:37:05 -0800

    setup routes and express body parser middleware

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const users = require("./routes/api/users");
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(express.json({ extended: false }));
 
-app.get("/users", users.getUsers);
-app.get("/users/:id", users.getUserById);
-app.post("/users", users.createUser);
-app.put("/users/:id", users.updateUser);
-app.delete("/users/:id", users.deleteUser);
-
-app.get("/", (req, res) => {
-  res.json({ info: "Node.js, Express, and Postgres API" });
-});
+// Define Routes
+app.use("/api/users", users);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
