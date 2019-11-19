8c98ed3c9deae8c962546e9225f49004ca7c7959
Mike Evlantev <mikeev21@gmail.com>
2019-11-18 20:33:59 -0800

    introduced api version based on package version

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+const majorVersion = process.env.npm_package_version.split(".")[0];
 
 const users = require("./routes/api/users");
 
 app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use("/api/users", users);
-
+app.use(`/api/v${majorVersion}/users`, users);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
